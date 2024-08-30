3c35f8221a1dc9190402f4e8b573bd1ad6a67114
h0996g <h0996g@gmail.com>
2024-08-30 17:20:20 +0100

    vercel done

--- app.js
+++ app.js
@@ -1,46 +1,54 @@
-// const sharp = require('sharp');
-
-
 const app = require('express')();
-const fs = require('fs');
-const path = require('path');
+const { put } = require('@vercel/blob');
 const Jimp = require('jimp');
+const path = require('path');
 
 app.set('trust proxy', true);
 const PORT = process.env.PORT || 3000;
-const logFilePath = path.join(__dirname, 'access.lo');
-app.use((req, res, next) => {
+
+
+app.use(async (req, res, next) => {
     const now = new Date();
-    log = `${now.toISOString()} - IP: ${req.ip}, Requested File  ${req.path}`;
-    fs.appendFile(logFilePath, log + "\n", (err) => {
-        if (err) {
-            console.error("Error writing log to file", err);
-        }
-        next();
+    const imageName = path.basename(req.path);
+    const ip = req.ip;
+    const log = `${now.toISOString()} - ${imageName} - IP: ${ip}`;
+
+    try {
+        await put(`logs/${imageName}.txt`, log, {
+            access: 'public',
+            contentType: 'text/plain'
+        });
+    } catch (error) {
+        console.error("Error writing log to Vercel Blob:", error);
     }
-
-
-    )
+    next();
 });
+
+
 app.get('/', (req, res) => {
     res.send('Hello visitor');
 });
+
 app.get('/image/:filename.png', (req, res) => {
     const width = 1;
     const height = 1;
 
     new Jimp(width, height, 0x00000000, (err, image) => {
-        if (err) throw err;
+        if (err) {
+            console.error("Error creating image:", err);
+            return res.status(500).send("Error creating image");
+        }
         image.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
-            if (err) throw err;
+            if (err) {
+                console.error("Error getting image buffer:", err);
+                return res.status(500).send("Error processing image");
+            }
             res.set('Content-Type', Jimp.MIME_PNG);
             res.send(buffer);
         });
     });
 });
-app.listen
-app.listen(PORT, () => {
-    console.log('Server started ');
 
-});;
-// });
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
